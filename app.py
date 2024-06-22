d5fb2db5ccfff29c59161efdaf29a192c328c4c3
Hiro Funatsuka <hiro.funatsuka@gmail.com>
2024-06-22 15:19:15 +0100

    Add CRUD operations

--- app.py
+++ app.py
@@ -170,6 +170,66 @@ def retrieve_password(email: str):
     else:
         return jsonify(message="That email doesn't exist"), 401
 
+
+@app.route('/planet_details/<int:planet_id>', methods=["GET"])
+def planet_details(planet_id: int):
+    planet = Planet.query.filter_by(id=planet_id).first()
+    if planet:
+        result = planet_schema.dump(planet)
+        return jsonify(result)
+    else:
+        return jsonify(message="That planet does not exist"), 404
+
+
+@app.route('/add_planet', methods=['POST'])
+@jwt_required()
+def add_planet():
+    name = request.form['name']
+    test = Planet.query.filter_by(name=name).first()
+    if test:
+        return jsonify(message='There is already a planet by that name'), 409
+    else:
+        type = request.form['type']
+        home_star = request.form['home_star']
+        mass = float(request.form['mass'])
+        radius = float(request.form['radius'])
+        distance = float(request.form['distance'])
+        planet = Planet(name=name, type=type, home_star=home_star,
+                        mass=mass, radius=radius, distance=distance)
+        db.session.add(planet)
+        db.session.commit()
+        return jsonify(message='Planet added successfully'), 201
+
+
+@app.route('/update_planet', methods=['PUT'])
+@jwt_required()
+def update_planet():
+    planet_id = int(request.form['planet_id'])
+    planet = Planet.query.filter_by(id=planet_id).first()
+    if planet:
+        planet.name = request.form['name']
+        planet.type = request.form['type']
+        planet.home_star = request.form['home_star']
+        planet.mass = float(request.form['mass'])
+        planet.radius = float(request.form['radius'])
+        planet.distance = float(request.form['distance'])
+        db.session.commit()
+        return jsonify(message='Planet updated successfully'), 202
+    else:
+        return jsonify(message='That planet does not exist'), 404
+
+
+@app.route('/remove_planet/<int:planet_id>', methods=['DELETE'])
+@jwt_required()
+def remove_planet(planet_id: int):
+    planet = Planet.query.filter_by(id=planet_id).first()
+    if planet:
+        db.session.delete(planet)
+        db.session.commit()
+        return jsonify(message='Planet removed successfully'), 202
+    else:
+        return jsonify(message='That planet does not exist'), 404
+
 # Database Models
 
 
